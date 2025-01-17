24ef28d4bc6daeabd158459a222e0a2961746858
stiliyan <stiliyan_2001@abv.bg>
2025-01-17 17:46:28 +0200

    Refactor user handling and add button functionality to display user details

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
 "use strict";
 
-let username = {
-  name: "Andrew",
-  email: "andrew@example.com",
-};
-// window.username = username;
+// function init(name, isTrue) {
+//     document.getElementById('output').innerHTML = `${username.name} (${username.email})`;
+// }
 
-username = {
-  name: "Ashley",
-  email: "ashley@example.com",
-};
+// function nameString() {
+//     return `${username.name} (${username.email})`;
+// }
 
-function init() {
-  // if (true) {
-  //     var email = 'andrew@example.com';
-  //     let email = 'andrew@example.com';
-  //     document.getElementById('output').innerHTML = email;
-  // }
+// const nameString = name => `${name} (${username.email})`;
 
-  document.getElementById(
-    "output"
-  ).innerHTML = `${username.name} (${username.email})`;
-}
+// document.getElementById('output').innerHTML = nameString('Andrew');
+
+let users = [
+  {
+    name: "Andrew",
+    email: "andrew@example.com",
+  },
+  {
+    name: "Ashley",
+    email: "ashley@example.com",
+  },
+];
+
+let names = [];
+
+// users.forEach(function(user) {
+//     // do something with that user object
+//     names.push(user.name);
+// });
+
+users.forEach((user) => names.push(user.name));
+
+document.getElementById("output").innerHTML = names.join(", ");
+
+document.getElementById("btn").addEventListener("click", (e) => {
+  const getDetails = () => {
+    return `The button id is ${e.currentTarget.getAttribute("id")}`;
+  };
+
+  document.getElementById("output").innerHTML = getDetails();
+});
